11f4fba7ea687bb2aafe19d2ea88c25188722e30
hikarut <seven_star_dust@yahoo.co.jp>
2017-01-28 13:10:46 +0900

    add sdk

---
+++ PWave/PWave-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PWave-Bridging-Header.h
+//  PWave
+//
+//  Created by HikaruTakahashi on 2017/01/28.
+//  Copyright © 2017年 hikaru. All rights reserved.
+//
+
+#ifndef PWave_Bridging_Header_h
+#define PWave_Bridging_Header_h
+
+#import "MWMDevice.h"
+#import "MWMDelegate.h"
+
+#endif /* PWave_Bridging_Header_h */
