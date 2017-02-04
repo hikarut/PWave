f2314c851e348f751327372faff604b032768af0
hikarut <seven_star_dust@yahoo.co.jp>
2017-02-04 22:57:58 +0900

    mod

--- PWave/Const.swift
+++ PWave/Const.swift
@@ -9,7 +9,7 @@
 struct Const {
     static let interval = 0.5
     
-    static let websocketServer = "ws://160.16.241.168:8080/"
+    static let websocketServer = "ws://160.16.241.168:8888/"
     static let websocketServerDebug = "ws://localhost:8080/"
     static let sendInterval = 0.1
     static let sendDataInit: [String: Any] = [
